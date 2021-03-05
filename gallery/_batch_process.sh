81b6c7d0939c4637763818b27ff20fa8d79e8f12
Claw Claws <clawclasw@gmail.com>
2021-03-05 17:29:45 +0000

    Add 4k thumbnails for anything screens above 1080p

--- gallery/_batch_process.sh
+++ gallery/_batch_process.sh
@@ -7,7 +7,10 @@ for filename in originals/*; do
 	# Originals as JPG.
 	convert "$filename" -quality 100 "$(basename "$filename" | cut -f 1 -d '.').jpg"
 	
-	# For a 1080p screen, file resolution should be roughly 442 pixels wide..
+	# For a 1080p screen, file resolution should be roughly 442 pixels wide.
 	convert "$filename" -quality 100 -resize 442x "$(basename "$filename" | cut -f 1 -d '.')_1080.jpg"
+
+	# For a 4k screen, file resolution should be roughly 884 pixels wide.
+	convert "$filename" -quality 100 -resize 884x "$(basename "$filename" | cut -f 1 -d '.')_4k.jpg"
 done
 
